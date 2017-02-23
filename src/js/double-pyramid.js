dc76ec9b3eab08bc0bbacd1d3b3db9c83cedb0f7
Vizdatalab <github@vizdatalab.com>
2017-02-23 15:36:08 -0500

    Include column order events to bind with buttons

--- src/js/double-pyramid.js
+++ src/js/double-pyramid.js
@@ -12,29 +12,32 @@ function widthCalc(id){
 };
 // end helper functions
 var double_pyramid = {
-    generate: function(data_url,graph_id,menu_id){
+    generate: function(data_url,graph_id,menu_id,load_callback){
 	var graph = {
 	    data_url: data_url,
 	    graph_id: graph_id,
 	    menu_id: menu_id,
-	    margin: {top: 20, right: 40, bottom: 10, left: 40},
+	    h_ratio: 0.6,
+	    margin: {top: 20, right: 10, bottom: 10, left: 55},
 	    lbl_space: 100,
-	    format: d3.format(".1%"),
+	    format: d3.format('.4s'),
 	    categories: undefined,
 	    menu: undefined,
 	    opts: undefined,
 	    svg: undefined,
 	    altKey: undefined,
 	    // Selector to order bars and create scales
+	    sel_options: ['perLeft','perRight'],
 	    selector: 'perLeft',
 	    // Number to select top data to visualize
 	    top_sel: 33,
-	    createAxis: function(scale){
+	    createAxis: function(scale,ticks){
 		return d3.svg.axis()
 		    .scale(scale)
 		    .orient("top")
 		    .tickSize(-this.height - this.margin.bottom)
-		    .tickFormat(this.format);
+		    .tickFormat(this.format)
+		    .ticks(ticks);
 	    },
 	    createBars: function(elem,classname){
 		var that = this;
@@ -61,7 +64,25 @@ var double_pyramid = {
 		    .attr('width', function(d){return that.xRight(d[data_sel]);});
 		upbar.select('.'+classname)
 		    .attr('x',function(d){
-			return offset+(isLeftColumn ? -(that.xRight(d[data_sel])+30):that.xRight(d[data_sel])+3);
+			var label_len= 20;
+			// ToDo: get the real equation to calculate label offset for negative x
+			switch(that.format(d[data_sel]).length){
+			case 4:
+			    label_len = 25;break;
+			case 5:
+			    label_len = 32;break;
+			case 6:
+			    label_len = 42;break;
+			case 7:
+			    label_len = 42;break;
+			case 8:
+			    label_len = 52;break;
+			default:
+			    break;
+			}
+			return offset+(isLeftColumn ?
+				       -(that.xRight(d[data_sel])+label_len) :
+				       that.xRight(d[data_sel])+3);
 		    })
 		    .text(function(d){return that.format(d[data_sel]);});
 		return upbar;
@@ -95,7 +116,7 @@ var double_pyramid = {
 		    .attr('y',that.y.rangeBand() / 2)
 		    .attr('dy','.35em');
 	    },
-	    loadData: function(url){
+	    loadData: function(url,callback){
 		var that = this;
 		d3.csv(data_url, function(error,data) {
 		    if (error) console.log(error);
@@ -106,8 +127,8 @@ var double_pyramid = {
 			var total_left = d3.sum(that.categories[d], function (d){return d.left});
 			var total_right = d3.sum(that.categories[d], function (d){return d.right});
 			that.categories[d].forEach(function(row){
-			    row['perLeft'] = row['left'] / total_left;
-			    row['perRight'] = row['right'] / total_right;
+			    row['perLeft'] = row['left'];
+			    row['perRight'] = row['right'];
 			});
 			that.opts.push({'cod':d,'title':that.categories[d][0]['category_name']});
 		    });
@@ -117,18 +138,32 @@ var double_pyramid = {
 			.enter().append('option')
 			.text(function(d){return d.title;})
 			.attr('value',function(d){return d.cod});
-		    // Set option selection
+		    // Set option selection to start
 		    that.menu.property('value',Object.keys(that.categories)[0]);
 		    that.redraw(that);
+		    
+		    if(callback) callback(that);
 		});
 	    },
 	    // Change event for menu
-	    change: function(graph) {
+	    change: function(graph,selector) {
 		clearTimeout(this.timeout);
 		d3.transition()
 		    .duration(graph.altKey ? 7500 : 750)
 		    .each(function(){graph.redraw(graph)});
 	    },
+	    reorder: function(graph,selector) {
+		if(selector == undefined) return;
+		if(graph.sel_options.indexOf(selector) < 0) return;
+		clearTimeout(this.timeout);
+		d3.transition()
+		    .duration(graph.altKey ? 7500 : 750)
+		    .each(function(){
+			graph.selector = selector;
+			graph.redraw(graph)
+		    });
+	    },
+	    
 	    // Draw/redraw elements
 	    redraw: function(graph) {
 		var sel = graph.menu.property('value');
@@ -151,9 +186,12 @@ var double_pyramid = {
 		graph.createDataLabels(barEnterRight,'perRight',graph.offset_right,'valueRight');
 		graph.createDataLabels(barEnterLeft,'perLeft',0,'valueLeft');
 		
-		// TODO: Find max on both scales
-		graph.xRight.domain([0, top[0][graph.selector]]);
-		graph.xLeft.domain([0,top[0][graph.selector]]);
+		// Find max on both scales
+		var max_left = d3.max(top,function(d){return parseFloat(d['perLeft'])});
+		var max_right = d3.max(top,function(d){return parseFloat(d['perRight'])});
+		var max_axis = d3.max([max_left,max_right]);
+		graph.xRight.domain([0,max_axis]);
+		graph.xLeft.domain([0,max_axis]);
 		
 		// Set update transition for bars
 		graph.updateBars(barRight,'perRight',graph.offset_right,'valueRight',false);
@@ -171,8 +209,9 @@ var double_pyramid = {
 	    },
 	    setInitVars: function(){
 		// Calculate variables to render
+		this.margin.right = 55; // Hack to work (??)
 		this.width = widthCalc(graph_id) - this.margin.left - this.margin.right;
-		this.height = (this.width*0.75) - this.margin.top - this.margin.bottom;
+		this.height = (this.width*this.h_ratio) - this.margin.top - this.margin.bottom;
 		this.mid_pnt = (this.width-this.lbl_space)/2;
 		this.offset_right = this.mid_pnt+this.lbl_space;
 		this.offset_left = this.mid_pnt-3;
@@ -180,9 +219,14 @@ var double_pyramid = {
 		this.xRight = d3.scale.linear().range([0, this.mid_pnt]);
 		this.xLeft = d3.scale.linear().range([this.mid_pnt,0]);
 		this.y = d3.scale.ordinal().rangeRoundBands([0, this.height], .1);
-		
-		this.xAxisRight = this.createAxis(this.xRight);
-		this.xAxisLeft = this.createAxis(this.xLeft);
+
+		// Create axis
+		var ticks;
+		if(this.width < 400) ticks = 1;
+		else if (this.width < 750) ticks = 3;
+		else ticks = 6;
+		this.xAxisRight = this.createAxis(this.xRight,ticks);
+		this.xAxisLeft = this.createAxis(this.xLeft,ticks);
 
 		// Create object
 		graph.menu = d3.select('#'+graph.menu_id+' select');
@@ -212,13 +256,11 @@ var double_pyramid = {
 		this.redraw(this);
 	    }
 	};
-	// ... to be continue
 	graph.setInitVars();
-			
+	
 	// Arrange data
-	graph.loadData(data_url);
-	graph.timeout =  setTimeout(function() {
-	    graph.menu.property('value',Object.keys(graph.categories)[0]).node().focus();
+	graph.loadData(data_url,load_callback);
+	graph.timeout = setTimeout(function() {
 	    graph.change(graph);
 	}, 5000)
 	
@@ -230,13 +272,16 @@ var double_pyramid = {
 	return graph;
     }
 }
-
 var oas;
-$(document).ready(function(){
-    var graph = double_pyramid.generate('data/rnd_data.csv','graph','menu');
-    oas = graph;
-    window.onresize = function(event){
-	graph.resize();
-    }
+document.addEventListener("DOMContentLoaded",function(event){
+    var graph = double_pyramid.generate('data/rnd_data.csv','graph','menu',function(graph){
+	d3.select('#order-selector').selectAll('button')
+	    .on('click',function(){
+		d3.select('#order-selector').selectAll('button')
+		    .classed('active',false);
+		this.classList.add('active');
+		graph.reorder(graph,this.value);
+	    });
+	window.onresize = function(event){graph.resize();}
+    });
 });
-
